ee2e64006685d858f5399252891f6afd7cdaa1ae
王萍 <wang.03.25@qq.com>
2017-01-10 21:46:58 +0800

    结束

--- testWord/src/com/word/TestJacob.java
+++ testWord/src/com/word/TestJacob.java
@@ -1,50 +1,50 @@
-package com.word;
-
-import com.jacob.activeX.ActiveXComponent;
-import com.jacob.com.Dispatch;
-import com.jacob.com.Variant;
-
-public class TestJacob {
-
-	
-	public static void extractDoc(String inputFIle, String outputFile) { 
-		   boolean flag = false; 
-		   
-		   // 打开Word应用程序 
-		   ActiveXComponent app = new ActiveXComponent("Word.Application"); 
-		   try { 
-		      // 设置word不可见 
-		      app.setProperty("Visible", new Variant(false)); 
-		      // 打开word文件 
-		      Dispatch doc1 = app.getProperty("Documents").toDispatch(); 
-		      Dispatch doc2 = Dispatch.invoke( 
-		            doc1, 
-		            "Open", 
-		            Dispatch.Method, 
-		            new Object[] { inputFIle, new Variant(false), 
-		                  new Variant(true) }, new int[1]).toDispatch(); 
-		      // 作为txt格式保存到临时文件 Variant(7)
-		      // 作为html格式保存到临时文件 Variant(8)
-		      Dispatch.invoke(doc2, "SaveAs", Dispatch.Method, new Object[] { 
-		            outputFile, new Variant(8) }, new int[1]); 
-		      // 关闭word 
-		      Variant f = new Variant(false); 
-		      Dispatch.call(doc2, "Close", f); 
-		      flag = true; 
-		   } catch (Exception e) { 
-		      e.printStackTrace(); 
-		   } finally { 
-		      app.invoke("Quit", new Variant[] {}); 
-		   } 
-		   if (flag == true) { 
-		      System.out.println("Transformed Successfully"); 
-		   } else { 
-		      System.out.println("Transform Failed"); 
-		   } 
-		} 
-	public static void main(String[] args) {
-		extractDoc("d:/test.doc","d:/test.htm");
-
-	}
-
-}
+//package com.word;
+//
+//import com.jacob.activeX.ActiveXComponent;
+//import com.jacob.com.Dispatch;
+//import com.jacob.com.Variant;
+//
+//public class TestJacob {
+//
+//
+//	public static void extractDoc(String inputFIle, String outputFile) {
+//		   boolean flag = false;
+//
+//		   // 打开Word应用程序
+//		   ActiveXComponent app = new ActiveXComponent("Word.Application");
+//		   try {
+//		      // 设置word不可见
+//		      app.setProperty("Visible", new Variant(false));
+//		      // 打开word文件
+//		      Dispatch doc1 = app.getProperty("Documents").toDispatch();
+//		      Dispatch doc2 = Dispatch.invoke(
+//		            doc1,
+//		            "Open",
+//		            Dispatch.Method,
+//		            new Object[] { inputFIle, new Variant(false),
+//		                  new Variant(true) }, new int[1]).toDispatch();
+//		      // 作为txt格式保存到临时文件 Variant(7)
+//		      // 作为html格式保存到临时文件 Variant(8)
+//		      Dispatch.invoke(doc2, "SaveAs", Dispatch.Method, new Object[] {
+//		            outputFile, new Variant(8) }, new int[1]);
+//		      // 关闭word
+//		      Variant f = new Variant(false);
+//		      Dispatch.call(doc2, "Close", f);
+//		      flag = true;
+//		   } catch (Exception e) {
+//		      e.printStackTrace();
+//		   } finally {
+//		      app.invoke("Quit", new Variant[] {});
+//		   }
+//		   if (flag == true) {
+//		      System.out.println("Transformed Successfully");
+//		   } else {
+//		      System.out.println("Transform Failed");
+//		   }
+//		}
+//	public static void main(String[] args) {
+//		extractDoc("d:/test.doc","d:/test.htm");
+//
+//	}
+//
+//}
--- testWord/src/com/word/TestPOI.java
+++ testWord/src/com/word/TestPOI.java
@@ -1,60 +1,60 @@
-package com.word;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFDateUtil;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.junit.Ignore;
-import org.junit.Test;
-
-
-public class TestPOI {
-	
-	@Test
-	@Ignore
-	public void testRead1(){
-		
-		File file=new File("d:/test.doc");
-		try {
-			FileInputStream fis=new FileInputStream(file);
-			WordExtractor  wordExtractor=new WordExtractor(fis);
-			System.out.println(wordExtractor.getText());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-	}
-	/**
-	 * 分段读操作
-	 */
-	@Test 
-	public void testPara(){
-		
-		File file=new File("d:/test.doc");
-		try {
-			FileInputStream fis=new FileInputStream(file);
-			WordExtractor  wordExtractor=new WordExtractor(fis);
-			String [] paras=wordExtractor.getParagraphText();
-			for(int i=0;i<paras.length;i++){
-				System.out.print("第"+i+"段-->");
-				System.out.println(paras[i]);
-			}
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-}
+//package com.word;
+//
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//
+//import org.apache.poi.hssf.usermodel.HSSFCell;
+//import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+//import org.apache.poi.hssf.usermodel.HSSFRow;
+//import org.apache.poi.hssf.usermodel.HSSFSheet;
+//import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+//import org.apache.poi.hwpf.extractor.WordExtractor;
+//import org.junit.Ignore;
+//import org.junit.Test;
+//
+//
+//public class TestPOI {
+//
+//	@Test
+//	@Ignore
+//	public void testRead1(){
+//
+//		File file=new File("d:/test.doc");
+//		try {
+//			FileInputStream fis=new FileInputStream(file);
+//			WordExtractor  wordExtractor=new WordExtractor(fis);
+//			System.out.println(wordExtractor.getText());
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//
+//
+//	}
+//	/**
+//	 * 分段读操作
+//	 */
+//	@Test
+//	public void testPara(){
+//
+//		File file=new File("d:/test.doc");
+//		try {
+//			FileInputStream fis=new FileInputStream(file);
+//			WordExtractor  wordExtractor=new WordExtractor(fis);
+//			String [] paras=wordExtractor.getParagraphText();
+//			for(int i=0;i<paras.length;i++){
+//				System.out.print("第"+i+"段-->");
+//				System.out.println(paras[i]);
+//			}
+//
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
+//
+//}
